ed943d14333e6e9b1d019af6ecc1bd5dead9f742
copiner <810023494@qq.com>
2022-05-23 11:18:45 +0800

    init

--- nodeapp/src/index.js
+++ nodeapp/src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./persistence');
-
 const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
-app.use(require('body-parser').json());
+app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
 app.get('/items', getItems);
